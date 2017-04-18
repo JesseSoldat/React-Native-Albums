0e48085292788afbdb9c81b203fc54e9d631f57b
jesse <jsoldat@hotmail.com>
2017-04-18 10:03:13 +0700

    finished button

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button';
 
 
 const AlbumDetail = ({album}) => {
@@ -31,6 +32,11 @@ const AlbumDetail = ({album}) => {
 				<Image 
 					style={imageStyle}
 					source={{uri: image}} />
+			</CardSection>
+			<CardSection>
+				<Button onPress={() => Linking.openURL(url)}>
+					Buy Now
+				</Button>
 			</CardSection>
 		</Card>
 	);
